24d0eaa5f587889b5ec312ede0ecaeb1418f8232
unknown <micheldejesus@gmail.com>
2015-05-25 18:27:41 -0300

    Iniciando o cadastro de empresas

--- gs-rest-service-complete/src/main/java/hello/EmpresaController.java
+++ gs-rest-service-complete/src/main/java/hello/EmpresaController.java
@@ -1,6 +1,10 @@
 package hello;
 
+import io.undertow.attribute.RequestMethodAttribute;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import delivery.model.Empresa;
@@ -8,10 +12,12 @@ import delivery.model.Empresa;
 @RestController
 public class EmpresaController {
 	
-	@RequestMapping("/cadastrarEmpresa")
-	public Empresa cadastrarEmpresaController(){
+	@RequestMapping(value="/cadastrarEmpresaInicio" , method=RequestMethod.GET)
+	public Empresa cadastrarEmpresaController(	@RequestParam(value="nome")String nome,
+												@RequestParam(value="cpfCnpj")int cpfCnpj){
 		Empresa empresa = new Empresa();
-		empresa.setNome("Habibs");
+		empresa.setNome(nome);
+		empresa.setCpfCnpj(cpfCnpj);
 		return empresa;
 	}
 }
